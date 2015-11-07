493ec76202e3d4dfd5ccd52438b28938a86f279c
Paul <mail@paulcbauer.eu>
2015-11-07 14:09:13 +0100

    Add descriptions file/license and links.

--- ui.R
+++ ui.R
@@ -24,7 +24,17 @@ library(car)
       selectInput("transformation", label = "Transformation:",
                   choices = c("none", "center", "divided by 10", "mininum to 0", "reverse", "standardize"), selected = 1),
       selectInput("original.scale", label = "Keep original X-scale:",
-                  choices = c("no", "yes (-50 to 170)"), selected = 1)   
+                  choices = c("no", "yes (-50 to 170)"), selected = 1),
+      br(), br(),
+      
+      div("Shiny app by", 
+          a(href="http://paulcbauer.eu/",target="_blank", 
+            "Paul C. Bauer"),align="right", style = "font-size: 8pt"),
+      
+      
+      div("Shiny/R code:",
+          a(href="https://github.com/paulbauer/transformations_of_variables/",
+            target="_blank","GitHub"),align="right", style = "font-size: 8pt")
 
   ),
 
